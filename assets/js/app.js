ae52a2f5c1e8765ce76e886c5e0b1a569225f97f
rahmatagungj <rahmatagungj@gmail.com>
2021-03-25 20:55:22 +0700

    fix methods

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,16 +11,20 @@ new Vue({
             if (itemValid) {
                 this.todos.push({ text: itemValid,done: false})
                 this.newTodo = '';
+                localStorage.setItem('lists',JSON.stringify(this.todos))
             } else {
                 console.info("data tidak valid")
             }
         },
         removeItem: function(index) {
             this.todos.splice(index,1)
-            // console.log(index);
+            localStorage.setItem('lists',JSON.stringify(this.todos))
         },
         toggleC: function(todo){
             todo.done = !todo.done
         }
+    },
+    mounted: function() {
+        this.todos = JSON.parse(localStorage.getItem('lists'))
     }
 })
